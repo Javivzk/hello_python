92fa8c384221724e1e7730d8b37f2e90f5384e82
Javivzk <javi.vzk@gmail.com>
2023-06-20 20:18:33 +0200

    Llegada a los operadores comparativos

--- Hello/02_operators.py
+++ Hello/02_operators.py
@@ -1,7 +1,44 @@
-### Operadores ###
+### Operadores Aritmeticos ###
 
+# Operaciones con enteros
 print(3 + 4)
 print(3 - 4)
 print(3 * 4)
-print(10 % 2)
+print(10 % 3) # Modulo (Resto)
+print(10 // 3) # Siempre da numero entero
+print(10 / 3) # Division normal
+print(2 ** 3) # Calcular un exponente
+print(2 ** 3 + 3 - 7 // 4)
 
+# Operaciones con cadenas de texto
+print("Hola " + "Python " + "¿Qué tal?")
+print("Hola " + str(5)) # Combinar varios tipos
+# Operaciones mixtas
+
+print("Hola " * (2 ** 3))
+print("Hola " * 5) # Multiplica la palabra entre comillas
+
+my_float = 2.5 * 2
+print("Hola " * int(my_float))
+
+### Operadores Comparativos ###
+
+# Operaciones con enteros
+print(3 > 4)
+print(3 < 4)
+print(3 >= 4)
+print(4 <= 4)
+print(3 == 4)
+print(3 != 4)
+print(3 > 4 == 2)
+
+# Operaciones con cadenas de texto
+print("Hola" > "Python")
+print("Hola" < "Python")
+print("aaaa" >= "abaa")  # Ordenación alfabética por ASCII
+print(len("aaaa") >= len("abaa"))  # Cuenta caracteres
+print("Hola" <= "Python")
+print("Hola" == "Hola")
+print("Hola" != "Python")
+
+### Operadores Lógicos ###
